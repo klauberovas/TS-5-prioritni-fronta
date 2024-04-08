24f00d0d61a1c9de8042ad374860f0517cc3e08f
Šárka Klauberová <sarkaa.novakova@seznam.cz>
2024-04-08 16:08:01 +0200

    basic done

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-console.log('Hello world');
+console.log('Ahoj');
+//fce přidání úkolů
+var enqueue = function (arr, value, priority) {
+    arr.push({ value: value, priority: priority });
+    return arr;
+};
+//přidání úkolů
+var priorityQueue = [];
+//low= 1, medium=2, high= 3
+console.log(enqueue(priorityQueue, 'Task 1', 1));
+console.log(enqueue(priorityQueue, 'Task 2', 2));
+console.log(enqueue(priorityQueue, 'Task 3', 3));
+console.log(enqueue(priorityQueue, 'Task 4', 3));
+//fce  vrácení prvku s nejvyšší prioritou
+var dequeue = function (arr) {
+    if (arr.length === 0) {
+        return null;
+    }
+    var highestPriorityIndex = 0;
+    for (var i = 1; i < arr.length; i++) {
+        if (arr[i].priority > arr[highestPriorityIndex].priority) {
+            highestPriorityIndex = i;
+        }
+    }
+    return arr.splice(highestPriorityIndex, 1)[0];
+};
+var dequeueWithOption = function (arr) {
+    if (arr.length === 0) {
+        return { kind: 'none' };
+    }
+    var highestPriorityIndex = 0;
+    for (var i = 1; i < arr.length; i++) {
+        if (arr[i].priority > arr[highestPriorityIndex].priority) {
+            highestPriorityIndex = i;
+        }
+    }
+    var value = arr.splice(highestPriorityIndex, 1)[0];
+    return { kind: 'some', value: value };
+};
+// console.log(dequeue(priorityQueue));
+// console.log(dequeue(priorityQueue));
+// console.log(dequeue(priorityQueue));
+console.log(dequeueWithOption(priorityQueue));
+console.log(dequeueWithOption(priorityQueue));
+console.log(dequeueWithOption(priorityQueue));
+console.log(dequeueWithOption(priorityQueue));
+console.log(dequeueWithOption(priorityQueue));
--- index.ts
+++ index.ts
@@ -1,1 +1,78 @@
-console.log('Hello world');
+console.log('Ahoj');
+//objekt s úkolem
+interface QueueItem<T> {
+  value: T;
+  priority: number;
+}
+
+//Prioritní fronta
+type PriorityQueue<T> = QueueItem<T>[];
+
+//fce přidání úkolů
+const enqueue = <T>(
+  arr: PriorityQueue<T>,
+  value: T,
+  priority: number,
+): PriorityQueue<T> => {
+  arr.push({ value, priority });
+  return arr;
+};
+
+//přidání úkolů
+const priorityQueue: PriorityQueue<string> = [];
+//low= 1, medium=2, high= 3
+console.log(enqueue(priorityQueue, 'Task 1', 1));
+console.log(enqueue(priorityQueue, 'Task 2', 2));
+console.log(enqueue(priorityQueue, 'Task 3', 3));
+console.log(enqueue(priorityQueue, 'Task 4', 3));
+
+//fce  vrácení prvku s nejvyšší prioritou
+const dequeue = <T>(arr: PriorityQueue<T>): QueueItem<T> | null => {
+  if (arr.length === 0) {
+    return null;
+  }
+
+  let highestPriorityIndex = 0;
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i].priority > arr[highestPriorityIndex].priority) {
+      highestPriorityIndex = i;
+    }
+  }
+
+  return arr.splice(highestPriorityIndex, 1)[0];
+};
+
+interface Some<T> {
+  kind: 'some';
+  value: QueueItem<T>;
+}
+
+interface None {
+  kind: 'none';
+}
+
+type Option<T> = Some<T> | None;
+const dequeueWithOption = <T>(arr: PriorityQueue<T>): Option<T> => {
+  if (arr.length === 0) {
+    return { kind: 'none' };
+  }
+  let highestPriorityIndex = 0;
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i].priority > arr[highestPriorityIndex].priority) {
+      highestPriorityIndex = i;
+    }
+  }
+
+  const value = arr.splice(highestPriorityIndex, 1)[0];
+  return { kind: 'some', value };
+};
+
+// console.log(dequeue(priorityQueue));
+// console.log(dequeue(priorityQueue));
+// console.log(dequeue(priorityQueue));
+
+console.log(dequeueWithOption(priorityQueue));
+console.log(dequeueWithOption(priorityQueue));
+console.log(dequeueWithOption(priorityQueue));
+console.log(dequeueWithOption(priorityQueue));
+console.log(dequeueWithOption(priorityQueue));
